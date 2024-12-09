9f6abc6f0e4911f7b8e86a3147be4a0e903df1c2
Confidence <nkeeonyeconfidence@gmail.com>
2024-12-09 16:47:15 +0100

    committing the calculation for area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Define the variables to calculate the area of a rectangle
+length = 10
+width = 5
+
+# Calculating the area of a rectangle
+area = length * width
+
+# Print the area
+print(f"The area of the rectangle is: {area}")
