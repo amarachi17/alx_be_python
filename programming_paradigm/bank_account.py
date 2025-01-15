9e752f4258b294f57d753d3680a005a4439c18b5
confidence <confidencenkeonye@gmail.com>
2025-01-15 17:36:36 +0100

    Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        # Initialize the bank account with an optional initial
+        self.__account_balance = initial_balance
+        
+    def deposit(self, amount):
+        # Add the specified amount to the account balance
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            print("Deposited amount must be greater than zero")
+        
+    def withdraw(self, amount):
+        # Deducting the amount from the account
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        # Printing the current account balance
+        print(f"Current account balance: ${self.__account_balance: .2f}")
